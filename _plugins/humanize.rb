183af23be57fd97c0ce2a640f6f6f24d65d49fab
sohanisharma <107360706+sohanisharma@users.noreply.github.com>
2022-07-12 12:59:56 +0530

    Add files via upload

--- _plugins/humanize.rb
+++ _plugins/humanize.rb
@@ -1,24 +1,9 @@
 module Jekyll
-
     module Humanize
-
-      def intcomma(value, delimiter=",")
-  
-        begin
-          orig = value.to_s
-          delimiter = delimiter.to_s
-        rescue Exception => e
-          puts "#{e.class} #{e}"
-          return value
-        end
-  
-        copy = orig.strip
-        copy = orig.gsub(/^(-?\d+)(\d{3})/, "\\1#{delimiter}\\2")
-        orig == copy ? copy : intcomma(copy, delimiter)
+      def intcomma(value)
+        return value.to_s.gsub(/^(-?\d+)(\d{3})/, "\\1,\\2")
       end
-  
     end
-  
   end
   
   Liquid::Template.register_filter(Jekyll::Humanize)
